c3202071f226a05125a626f845d04e4e10e4062b
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-01-26 19:09:52 +0530

    Create sum-of-two-numbers.c

---
+++ Variables/sum-of-two-numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a,b;
+    printf("Enter number a\n");
+    scanf("%d", &a);
+    printf(" Enter number b\n");
+    scanf("%d",&b);
+    printf(" The sum is %d\n", a+b);
+
+    return 0;
+
+}
+
+
+/*Output
+
+Enter number a
+4
+Enter number b
+6
+The sum is
+10
