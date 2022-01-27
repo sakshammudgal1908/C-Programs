3cb5b696b02b07456dd1ae6ad1253cf1f16316b5
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-01-27 19:39:55 +0530

    Create multiplication2.c

---
+++ Variables/multiplication2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,b,c;
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+    printf("Enter the value of b\n");
+    scanf("%d",&b);
+    printf("The Product of a and b is  \n");
+    printf("c = %d ", c=a*b); 
+
+}
+
+OUTPUT
+Enter the value of a
+4
+Enter the value of b
+6
+The Product of a and b is
+c=24
