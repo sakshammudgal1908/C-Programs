5f579128447144777e1462c3dfb33c1f937117ce
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-01-28 09:56:14 +0530

    Create Subtraction.c

---
+++ Variables/Subtraction.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+    printf("Enter the value of b\n");
+    scanf("%d",&b);
+    printf("The Subtraction is %d\n",a-b);
+
+}
+
+OUTPUT
+
+
+Enter the value of a
+9
+Enter the value of b
+6
+The Subtraction is 3
