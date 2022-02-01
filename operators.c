f0c9fa360315a33079e283514da9b22091b44ca6
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-01 19:40:58 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=25;
+    b=8;
+    printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n",a-b);;
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b);
+    printf("a%b=%d\n",a%b);
+}
+
+OUTPUT
+
+a+b=33
+a-b=17
+a*b=200
+a/b=3
+ab=1
