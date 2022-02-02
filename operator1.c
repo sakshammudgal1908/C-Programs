e146caa89a12e737b67272bf880b6d27b1421e84
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-02 19:06:26 +0530

    Create operator1.c

---
+++ operator1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    float b;
+    a=6;
+    b=2.12;
+    printf("a+b=%f\n",a+b);
+    printf("a-b=%f\n",a-b);
+    printf("a/b=%f\n",a/b);
+
+}
+
+OUTPUT
+
+a+b=8.12
+a-b=3.88
+a/b=2.83
