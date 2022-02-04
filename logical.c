fabd304f90ace3554d29232e1cf4efd9569c7fd1
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-04 19:14:58 +0530

    Create logical.c

---
+++ logical.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=10;
+    b=10;
+    printf("a && b = %d\n",a&&b);
+}
+
+OUTPUT
+
+a && b = 1
+  
+  
+ #include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=10;
+    b=0;
+    printf("a && b = %d\n",a&&b);
+}
+
+OUTPUT
+
+a && b = 0
