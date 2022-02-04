2ad21376c35ad08a43c2052191519fb5ff7be186
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-04 19:19:45 +0530

    Create logical_OR.c

---
+++ logical_OR.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=10;
+    b=45;
+    printf("a || b = %d\n", a||b);
+}
+
+OUTPUT
+
+a || b = 1
+  
+  
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=0;
+    b=0;
+    printf("a || b = %d\n", a||b);
+}
+
+OUTPUT
+
+a || b = 0
+  
+  
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    a=0;
+    b=45;
+    printf("a || b = %d\n", a||b);
+}
+
+OUTPUT
+
+a || b = 1
