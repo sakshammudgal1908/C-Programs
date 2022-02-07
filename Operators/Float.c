d3170d111f7af800184e9037f15a1620f698e092
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-07 21:50:37 +0530

    Create Float.c

---
+++ Operators/Float.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main( )
+{
+  
+  float num1,num2,sum;
+  
+ 
+  
+  printf("Enter first number: ");
+  scanf("%f",&num1);
+
+  
+  printf("Enter second number: ");
+  scanf("%f",&num2); 
+
+  
+  sum=num1+num2;
+
+  
+  printf("Sum of two numbers = %f",sum);
+  getch( );
+}
+
+OUTPUT
+
+Enter first number: 3
+Enter second number: 4.12
+Sum of two numbers = 7.12
