23594cc79e82953d215b869d1c75d2a0467a959a
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-02-08 09:51:44 +0530

    Create format_specifiers.c

---
+++ format_specifiers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a = 2;
+    float b = 4.46;
+    printf("The value of a is = %d and the vlue of b is = %f\n", a , b);
+    return 0;
+
+}
+
+
+OUTPUT
+
+
+The value of a is = 2 and the vlue of b is = 4.46
