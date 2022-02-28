f644f8af75d8323d11bcc267839ddc633ae41d40
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-03-01 00:13:04 +0530

    Create if_else2.c

---
+++ if_else2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    if (3>6)
+    printf("The condition is True");
+    else
+    printf("The condition is False");
+}
+
+
+OUTPUT
+
+The condition is False
