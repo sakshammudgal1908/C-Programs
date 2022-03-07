308100d0b35503f9bbc43c7b7b0f3c9a7c6b1ed0
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-03-07 23:56:56 +0530

    Create while.c

---
+++ while.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+}
+
+OUTPUT
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
