1d9da8ce32c5130783607a03fe82c5cb79c3f6cc
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-03-08 23:12:21 +0530

    Create do_while.c

---
+++ do_while.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    i=2;
+    do{
+        printf("%d\n",i);
+        i++;
+    }while(i<=20);
+}
+
+OUTPUT
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
