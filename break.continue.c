25e8bc7ff42201546ed71298840d4521268f857c
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2022-03-23 23:11:23 +0530

    Create break.continue.c

---
+++ break.continue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        if(i==6)
+        {
+            break;
+        }
+        printf("%d",i);
+    }   
+}
+
+
+
+OUTOUT
+
+012345
